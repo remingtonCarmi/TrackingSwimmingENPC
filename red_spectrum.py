4e3330bf1e8099a6709db94ef58974094598fa1d
Maxime Brisinger <maximebrisinger@gmail.com>
2020-03-10 16:45:29 +0100

    red_spectrum plus propre et requirements crée dossiers test et videos

--- red_spectrum.py
+++ red_spectrum.py
@@ -1,5 +1,5 @@
 """
-This code allows to indentify a swimmer on an photo with the red spectrum method.
+This code allows to identify a swimmer on an photo with the red spectrum method.
 @author: Victoria Brami, Maxime Brisinger, Theo Vincent
 """
 
@@ -15,76 +15,64 @@ import scipy.ndimage as ndimage
 plt.rcParams['image.cmap'] = 'gray' 
 # by default, the grayscale images are displayed with the jet colormap: use grayscale instead
 
-NAME = 'framee3.jpg'
+NAME = "test//frame1.jpg"
 THRESHOLD = 0.1
 THRESHOLD_2 = 18
 FIG_SIZE = (10,10)
 
-def load_image_red(name, method=1): 
+def load_image_red(name, method=2):
     """
     Loads image named NAME and returns its red component as a numpy.ndarray
 
     Args:
-        name(string) : name of the file to load
+        :param name : string, name of the file to load
+        :param method: integer
     """
-    I = plt.imread(name)
+    image = plt.imread(name)
     if method == 1:
-        I = 100.*I[:,:,0] - 99.*I[:,:,2]
-        I = I.astype('float')/255 # just to scale the values of the image between 0 and 1 (instead of 0 255)
+        image = 100.*image[:,:,0] - 99.*image[:,:,2]
+        image = image.astype('float')/255 # just to scale the values of the image between 0 and 1 (instead of 0 255)
     if method == 2:
-        J = I[:,:,0]
-        K = I[:,:,2]
-        I = I[:,:,0] < -1
+        red_image = image[:,:,0]
+        blue_image = image[:,:,2]
+        image = image[:,:,0] < -1
 
-        for i in range(I.shape[0] - 1):
-            for j in range(I.shape[1] -1):
-                if J[i,j] > 100 and J[i,j] < 190 and K[i,j] < 200:
-                    I[i,j] = 1
+        for i in range(image.shape[0] - 1):
+            for j in range(image.shape[1] -1):
+                if 100 < red_image[i, j] < 190 and blue_image[i, j] < 200:
+                    image[i,j] = 1
 
-    return I
+    return image
 
-def compute_gradient(I, sigma=0):
+def compute_gradient(image, sigma=0):
     """
-    Computes the vertical, horizontal, and the norm of the gradient of an image I. 
+    Computes the vertical, horizontal, and the norm of the gradient of an image image.
     Returns them as three numpy.ndarray
 
     Args:
-        I(numpy.ndarray) : image we want the gradient of
+        image(numpy.ndarray) : image we want the gradient of
         
         sigma(integer) : value of the parameter of the gaussian filter the algorithm apply
     """
-    I = ndimage.gaussian_filter(I, sigma, mode = 'constant', cval = 0)
-    v = np.array([[1./9, 0, -1./9],
-               [2./9, 0, -2./9],
-               [1./9, 0, -1./9]])
-    h = np.array([[1./9, 2./9, 1./9],
-               [0, 0, 0],
-               [-1./9, -2./9, -1./9]])
-    Iv = ndimage.convolve(I, v)
-    Ih = ndimage.convolve(I, h)
-    In = np.sqrt(Iv**2 + Ih**2)
-    return (Iv, Ih, In)
+    image = ndimage.gaussian_filter(image, sigma, mode = 'constant', cval = 0)
+    y = np.array([[1./9, 0, -1./9],
+                  [2./9, 0, -2./9],
+                  [1./9, 0, -1./9]])
+    x = np.array([[1./9, 2./9, 1./9],
+                  [0, 0, 0],
+                  [-1./9, -2./9, -1./9]])
+    gradient_y = ndimage.convolve(image, y)
+    gradient_x = ndimage.convolve(image, x)
+    gradient_norm = np.sqrt(gradient_y**2 + gradient_x**2)
+    return gradient_y, gradient_x, gradient_norm
 
-I = load_image_red(NAME, 2)
-I_threshold = (I > THRESHOLD) * 255
-I_grady, I_gradx, I_gradnorm = compute_gradient(I_threshold, 3)
-#I_gradnorm = I_gradnorm > THRESHOLD_2
-#I_edges = I_gradnorm > THRESHOLD
-plt.figure(figsize=FIG_SIZE)
-plt.imshow(I)
-plt.figure(figsize=FIG_SIZE)
-plt.imshow(I_gradnorm)
-# plt.figure(figsize=FIG_SIZE)
-# plt.imshow(I_threshold)
-plt.show()
-
-def extreme_white_pixels(I):
-    I = I > THRESHOLD_2
-    y_min, x_min = I.shape[0] - 1, I.shape[1] - 1
+def extreme_white_pixels(image):
+    image = image > THRESHOLD_2
+    y_min, x_min = image.shape[0] - 1, image.shape[1] - 1
     y_max, x_max = 0, 0
-    for y in range(I.shape[0] - 1):
-        for x in range(I.shape[1] -1):
-            if I[y,x]:
+    for y in range(image.shape[0] - 1):
+        for x in range(image.shape[1] -1):
+            if image[y,x]:
                 if y < y_min:
                     y_min = y
                 if y > y_max:
@@ -95,16 +83,31 @@ def extreme_white_pixels(I):
                     x_max = x
     return (x_min, y_min),  (x_max, y_max)
 
-# Draw the rectangle
-extremes = extreme_white_pixels(I_gradnorm)
-size = (extremes[1][0] - extremes[0][0], extremes[1][1] - extremes[0][1])
+def keep_edges(name, method=2, figures=False):
+    image = load_image_red(name, method)
+    threshold_image = (image > THRESHOLD) * 255
+    gradient_y, gradient_x, gradient_norm = compute_gradient(threshold_image, 3)
 
-rectangle = plt.Rectangle(extremes[0], size[0], size[1], fc="none", ec="red")
+    if figures:
+        plt.figure(figsize=FIG_SIZE)
+        plt.imshow(I)
+        plt.figure(figsize=FIG_SIZE)
+        plt.imshow(I_gradnorm)
+        plt.show()
+    return gradient_y, gradient_x, gradient_norm
 
-# Plot the result
-I = plt.imread(NAME)
-plt.figure(figsize=FIG_SIZE) # this line is not necessary, but allows you to control the size of the displayed image
-plt.imshow(I)
+def draw_rectangle(gradient_norm):
+    # Draw the rectangle
+    extremes = extreme_white_pixels(gradient_norm)
+    size = (extremes[1][0] - extremes[0][0], extremes[1][1] - extremes[0][1])
 
-plt.gca().add_patch(rectangle)
-plt.show()
+    rectangle = plt.Rectangle(extremes[0], size[0], size[1], fc="none", ec="red")
+    plt.gca().add_patch(rectangle)
+
+if __name__ == "__main__":
+    # Plot the result
+    IMAGE = plt.imread(NAME)
+    plt.figure(figsize=FIG_SIZE)
+    plt.imshow(IMAGE)
+    draw_rectangle(keep_edges(NAME)[2])
+    plt.show()
--- requirements.py
+++ requirements.py
@@ -10,3 +10,11 @@ import os
 # download library for video processing
 
 os.system("pip install opencv-python")
+
+# create needed folders 
+
+if not os.path.exists("test"):
+    os.makedirs("test")
+
+if not os.path.exists("videos"):
+    os.makedirs("videos")
