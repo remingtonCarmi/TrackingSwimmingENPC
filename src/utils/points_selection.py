3c1842f601d43bbfb11820e79f96688f7db63180
Théo VINCENT <theo.vincent@eleves.enpc.fr>
2020-04-13 15:32:34 +0200

    Selection point

--- src/utils/points_selection.py
+++ src/utils/points_selection.py
@@ -16,17 +16,20 @@ class ImageSelection(QLabel):
         self.setFocusPolicy(True)
 
         # Points management
-        self.final_list_points = list_points
         self.list_point = np.zeros(20, dtype=QPoint)
         self.nb_points = 0
-        self.cursorPos = QPoint(0, 0)
-        self.pStart = QPoint(0, 0)
-        self.pEnd = QPoint(0, 0)
+        self.cursorPos = QPoint()
+        self.pStart = QPoint()
+        self.pEnd = QPoint()
 
         # Background management
         self.pix_map = pix_map
         self.size = size
         self.setFixedSize(size)
+        top_left = QPoint()
+        bottom_right = QPoint(size.width() - 1, size.height() - 1)
+        self.rect_in_image = QRect(top_left, bottom_right)
+        self.zoom_in = False
 
     def paintEvent(self, event):
         super().paintEvent(event)
@@ -60,15 +63,28 @@ class ImageSelection(QLabel):
     def update_points(self):
         # Add a point if the mouse did not move
         if self.pStart == self.pEnd:
-            self.list_point[self.nb_points] = self.pStart
+            # Withdraw the zoom and add the point
+            if self.zoom_in:
+                # Withdraw the zoom
+                self.setPixmap(self.pix_map.scaled(self.size, Qt.IgnoreAspectRatio))
+                self.zoom_in = False
+                # Add the point
+                self.list_point[self.nb_points] = self.point_in_image()
+                # Reset the rectangle
+                top_left = QPoint()
+                bottom_right = QPoint(self.size.width() - 1, self.size.height() - 1)
+                self.rect_in_image = QRect(top_left, bottom_right)
+            else:
+                self.list_point[self.nb_points] = self.pStart
             self.nb_points += 1
         # Zoom if the mouse mouved
         else:
             self.zoom()
+            self.zoom_in = True
 
     def draw_points(self, q_painter):
         # Draw a circle that follows the mouse
-        q_painter.setPen(Qt.black)
+        q_painter.setPen(Qt.white)
         if not self.cursorPos.isNull():
             q_painter.drawEllipse(self.cursorPos.x() - 2, self.cursorPos.y() - 2, 4, 4)
 
@@ -77,7 +93,9 @@ class ImageSelection(QLabel):
         for index_point in range(self.nb_points):
             x = self.list_point[index_point].x()
             y = self.list_point[index_point].y()
-            q_painter.drawEllipse(x - 2, y - 2, 4, 4)
+            if self.in_showed_image(x, y):
+                (x, y) = self.adapt_point(x, y)
+                q_painter.drawEllipse(x - 2, y - 2, 4, 4)
 
     def get_points(self):
         points = np.zeros((self.nb_points, 2))
@@ -90,17 +108,57 @@ class ImageSelection(QLabel):
         return points
 
     def zoom(self):
-        # Compute the rectangle for the zoom
-        top_left = QPoint(0, 0)
-        bottom_right = QPoint(0, 0)
-        top_left.setX(min(self.pStart.x(), self.pEnd.x()))
-        top_left.setY(min(self.pStart.y(), self.pEnd.y()))
-        bottom_right.setX(max(self.pStart.x(), self.pEnd.x()))
-        bottom_right.setY(max(self.pStart.y(), self.pEnd.y()))
-        rectangle = QRect(top_left, bottom_right)
-        print(rectangle)
-        zoom_pix = self.pix_map.copy(rectangle)
+        rect_in_screen = self.rectangle_in_screen()
+        self.rect_in_image = self.rectangle_in_image(rect_in_screen)
+
+        zoom_pix = self.pix_map.copy(self.rect_in_image)
         self.setPixmap(zoom_pix.scaled(self.size, Qt.IgnoreAspectRatio))
+
+    def rectangle_in_screen(self):
+        top_left_x = max(min(self.pStart.x(), self.pEnd.x()), 0)
+        top_left_y = max(min(self.pStart.y(), self.pEnd.y()), 0)
+        top_left = QPoint(top_left_x, top_left_y)
+
+        bottom_right_x = min(max(self.pStart.x(), self.pEnd.x()), self.size.width())
+        bottom_right_y = min(max(self.pStart.y(), self.pEnd.y()), self.size.height())
+        bottom_right = QPoint(bottom_right_x, bottom_right_y)
+
+        return QRect(top_left, bottom_right)
+
+    def rectangle_in_image(self, rectangle_zoom):
+        start_x = self.rect_in_image.topLeft().x()
+        start_y = self.rect_in_image.topLeft().y()
+
+        top_left_x = start_x + (rectangle_zoom.topLeft().x() / self.size.width()) * self.rect_in_image.width()
+        top_left_y = start_y + (rectangle_zoom.topLeft().y() / self.size.height()) * self.rect_in_image.height()
+        top_left = QPoint(int(top_left_x), int(top_left_y))
+
+        bottom_right_x = start_x + (rectangle_zoom.bottomRight().x() / self.size.width()) * self.rect_in_image.width()
+        bottom_right_y = start_y + (rectangle_zoom.bottomRight().y() / self.size.height()) * self.rect_in_image.height()
+        bottom_right = QPoint(int(bottom_right_x), int(bottom_right_y))
+
+        return QRect(top_left, bottom_right)
+
+    def point_in_image(self):
+        start_x = self.rect_in_image.topLeft().x()
+        start_y = self.rect_in_image.topLeft().y()
+
+        point_x = start_x + (self.pStart.x() / self.size.width()) * self.rect_in_image.width()
+        point_y = start_y + (self.pStart.y() / self.size.height()) * self.rect_in_image.height()
+
+        return QPoint(int(point_x), int(point_y))
+
+    def in_showed_image(self, x_coord, y_coord):
+        if self.rect_in_image.left() < x_coord < self.rect_in_image.right():
+            if self.rect_in_image.top() < y_coord < self.rect_in_image.bottom():
+                return True
+        return False
+
+    def adapt_point(self, x_coord, y_coord):
+        in_zoom_x = (x_coord - self.rect_in_image.topLeft().x()) * self.size.width() / self.rect_in_image.width()
+        in_zoom_y = (y_coord - self.rect_in_image.topLeft().y()) * self.size.height() / self.rect_in_image.height()
+
+        return in_zoom_x, in_zoom_y
 
 
 def initialize_points(main_layout, height):
