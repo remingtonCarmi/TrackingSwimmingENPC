511461dc6e9cd26ee163338a7a24acb30af55319
unknown <victoria.brami88@gmail.com>
2020-04-16 17:01:05 +0200

    modifications to have non streched output images

--- src/perspective/correction_perspective.py
+++ src/perspective/correction_perspective.py
@@ -26,60 +26,38 @@ def correct_perspective_img(img, src, dst, testing, display):
     """
     h, w = img.shape[:2]
     
-    #we verify the pointed points are relevant (for example the lines formed must be parallel ie norm of crossing is null)
-    point1 = src[0]
-    point2 = src[1]
-    point3 = src[2]
-    point4 = src[3]
-    vect1 = point1 - point2
-    vect2 = point3 - point4
-    vect3 = point1 - point4
-    vect4 = point1 - point3
-    norm12 = abs(vect1[0]*vect2[1] - vect2[0]*vect1[1])
-    norm34 = abs(vect3[0]*vect4[1] - vect3[0]*vect4[1])
-    
-    if norm12 < 0.01 and norm34 < 0.01:
-        # we find the transform matrix M thanks to the matching of the four points
-        M = cv2.getPerspectiveTransform(src, dst)
-        # warp the image to a top-down view
-        warped = cv2.warpPerspective(img, M, (w, h), flags=cv2.INTER_LINEAR)
-    
-        if testing:
-            if display:
-                f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
-                f.subplots_adjust(hspace=.2, wspace=.05)
-                ax1.imshow(img)
-                x = [src[0][0], src[2][0], src[3][0], src[1][0], src[0][0]]
-                y = [src[0][1], src[2][1], src[3][1], src[1][1], src[0][1]]
-                ax1.plot(x, y, color='red', alpha=0.4, linewidth=3, solid_capstyle='round', zorder=2)
-                ax1.set_ylim([h, 0])
-                ax1.set_xlim([0, w])
-                ax1.set_title('Original Image', fontsize=30)
-                ax2.imshow(cv2.flip(warped, 1))
-                ax2.set_title('Corrected Image', fontsize=30)
-                plt.show()
-                return cv2.flip(warped, 1)
-            else:
-                return cv2.flip(warped, 1)
+    # we find the transform matrix M thanks to the matching of the four points
+    M = cv2.getPerspectiveTransform(src, dst)
+    # warp the image to a top-down view
+    warped = cv2.warpPerspective(img, M, (w, h), flags=cv2.INTER_LINEAR)
+
+    if testing:
+        if display:
+            f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+            f.subplots_adjust(hspace=.2, wspace=.05)
+            ax1.imshow(img)
+            x = [src[0][0], src[2][0], src[3][0], src[1][0], src[0][0]]
+            y = [src[0][1], src[2][1], src[3][1], src[1][1], src[0][1]]
+            ax1.plot(x, y, color='red', alpha=0.4, linewidth=3, solid_capstyle='round', zorder=2)
+            ax1.set_ylim([h, 0])
+            ax1.set_xlim([0, w])
+            ax1.set_title('Original Image', fontsize=30)
+            ax2.imshow(cv2.flip(warped, 1))
+            ax2.set_title('Corrected Image', fontsize=30)
+            plt.show()
+            return cv2.flip(warped, 1)
         else:
-            return warped, M
+            return cv2.flip(warped, 1)
+    else:
+        return warped, M
     
 
 # We will first manually select the source points 
 # we will select the destination point which will map the source points in
 # original image to destination points in unwarped image
-src1 = np.float32([(20,     1),
-                  (540,  130),
-                  (20,    520),
-                  (570,  450)])
+
 
     
-dst2 = np.float32([(1500, 0),
-                  (0, 0),
-                  (1500, 750),
-                  (0, 750)])
-
-
 
 
 if __name__ == "__main__":
@@ -95,6 +73,16 @@ if __name__ == "__main__":
                       (points[2][0], points[2][1])
                       ])
     print(src)
+    
+    #choosing the right correction points to have non streched images
+    d_1 = np.sqrt((points[0][0] - points[3][0])**2 + (points[0][1] - points[3][1])**2)
+    d_2 = np.sqrt((points[1][0] - points[2][0])**2 + (points[1][1] - points[2][1])**2)
+    d = min(d_1, d_2)
+    
+    dst2 = np.float32([(1500, 0),
+                  (0, 0),
+                  (1500, d),
+                  (0, d)])
     print("Select four points to unwarp the perspective: they must form a quadrangle")
     new_im = correct_perspective_img(im, src, dst2, True, False)
 
