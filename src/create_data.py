55b31d76a56c03748133a60c4927865788afac5c
Maxime Brisinger <maximebrisinger@gmail.com>
2020-04-27 14:40:49 +0200

    Create data from the txt file

--- src/create_data.py
+++ src/create_data.py
@@ -1,14 +1,12 @@
 from src.utils.save_data.split_image import split_and_save
-from src.utils.extract_image import extract_image_video
-from src.calibration import calibrate_video
+from src.utils.calibration_from_txt import calibrate_from_txt
 
 import os
 import cv2
 from pathlib import Path
 
 
-def create_data(path_video, margin, nb_lines=10,
-                is_corrected=False,
+def create_data(path_video, path_txt, margin, nb_lines=10,
                 time_begin=0, time_end=-1, destination=Path("../output/images/")):
 
     name_video = path_video.parts[-1][: -4]
@@ -17,11 +15,8 @@ def create_data(path_video, margin, nb_lines=10,
     # Create the directory where to save the data
     os.makedirs(path_directory)
 
-    # Extract images from the video
-    if not is_corrected:
-        corrected_images = calibrate_video(path_video, time_begin, time_end, create_video=False, creat_txt=True)
-    else:
-        corrected_images = extract_image_video(path_video, time_begin, time_end)
+    # Calibrate and extract images from the corrected video
+    corrected_images = calibrate_from_txt(path_video, path_txt, time_begin, time_end)
 
     n = len(corrected_images)
 
@@ -32,8 +27,8 @@ def create_data(path_video, margin, nb_lines=10,
 
 
 if __name__ == "__main__":
-    PATH_VIDEO = Path("../../data/videos/vid0.mp4")
-    # PATH_VIDEO_CALIBRATED = Path("../output/videos/corrected_vid0.mp4")
+    PATH_VIDEO = Path("../data/videos/vid0.mp4")
+    PATH_TXT = Path("../data/labels/vid0.txt")
 
-    MARGIN = -10
-    create_data(PATH_VIDEO, MARGIN, is_corrected=False, time_begin=11, time_end=13)
+    MARGIN = 0
+    create_data(PATH_VIDEO, PATH_TXT, MARGIN, time_begin=11, time_end=12)
---
+++ src/utils/calibration_from_txt.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from src.utils.extractions.extract_image import extract_image_video
+from src.utils.perspective_correction.perspective_correction import get_top_down_image
+
+
+def calibrate_from_txt(path_video, path_txt, time_begin=0, time_end=-1):
+
+    file = open(path_txt, 'r')
+    lines = file.readlines()
+    homography = np.fromstring(lines[-2], dtype=float, sep=',')
+    file.close()
+
+    homography = np.reshape(homography, (3, 3))
+    print(homography)
+
+    # Get the images
+    print("Get the images ...")
+    list_images = extract_image_video(path_video, time_begin, time_end)
+    nb_images = len(list_images)
+
+    # Transform the images
+    print("Correction of images ...")
+    for index_image in range(nb_images):
+        list_images[index_image] = get_top_down_image(list_images[index_image], homography)
+
+    return list_images
