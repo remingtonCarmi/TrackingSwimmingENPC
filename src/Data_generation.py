f8fe29b67bc3422800a3664093236e4f5ab3cf93
unknown <victoria.brami88@gmail.com>
2020-04-28 08:51:33 +0200

    modifications (still does not work)

--- src/Data_generation.py
+++ src/Data_generation.py
@@ -3,8 +3,8 @@
 import tensorflow as tf
 from pathlib import Path
 import matplotlib.pyplot as plt
-import cv2
 import numpy as np
+import pandas as pd
 from src.load_data import load_data
 from src.head_coords_image_association import associate_head_coordinates_with_image
 
@@ -20,7 +20,7 @@ def convert_into_image_tensor(image, coords_h):
         coords_h: (tuple) contains the head coordinates
     """
     image = np.asarray(image, np.float32)
-    image = tf.pack(image)
+    #image = tf.pack(image)
     image = tf.image.convert_image_dtype(image, tf.float32)
     return image, coords_h
 
@@ -32,16 +32,16 @@ def augment(coords_h, image):
     """
 
     image, coords_h = convert_into_image_tensor(image, coords_h)
-    image = tf.image.convert_image_dtype(image, tf.float32) # Cast and normalize the image to [0,1]
-    print('ok1.5')
+    image = tf.image.convert_image_dtype(image, tf.float32)
     image1 = tf.image.random_flip_left_right(image)
     image2 = tf.image.random_saturation(image, 0, 3)
     image3 = tf.image.random_brightness(image, max_delta=0.5)
     image4 = tf.image.random_saturation(image, 1, 2)
     image5 = tf.image.random_brightness(image, max_delta=0.3)
-    image6 = tf.image.translate(image, [3,3])
+    #image6 = tf.image.translate(image, [3,3])
 
-    list_pairs = [[image, coords_h], [image1, coords_h], [image2, coords_h], [image3, coords_h], [image4, coords_h], [image5, coords_h], [image6, [coords_h[0]+3, coords_h[1]+3]]]
+
+    list_pairs = [[image, coords_h], [image1, coords_h], [image2, coords_h], [image3, coords_h], [image4, coords_h], [image5, coords_h]]
 
     return list_pairs
 
@@ -55,17 +55,15 @@ def visualize(original, augmented):
     plt.subplot(1, 2, 2)
     plt.title('Augmented image')
     plt.imshow(augmented)
+    plt.show()
 
 
-if __name__ == "main":
-    IMAGE_PATH = Path("../../data/lanes/")
-    DATA_PATH = Path("../../data/head_points/")
+if __name__ == "__main__":
+    print('ok')
+    IMAGE_PATH = Path("../data/lanes/f123_l4.jpg")
+    DATA_PATH = Path("../data/head_points/test_file.csv")
     data, coords = load_data(DATA_PATH)
-    image, coords_h = associate_head_coordinates_with_image(IMAGE_PATH, coords)
-    print('ok1')
-    list_pairs = augment(coords_h, IMAGE_PATH)
-    print('ok2')
-    print(list_pairs[0].eval())
-    # image_string = tf.io.read_file(IMAGE_PATH)
-    # image = tf.image.decode_jpeg(image_string, channels=3)
-    # visualize(image, image)
+    image, coords_h = associate_head_coordinates_with_image(IMAGE_PATH, data)
+    list_pairs = augment(coords_h, image)
+    print(list_pairs[0][0].eval())
+
