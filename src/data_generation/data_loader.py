f2244b9091542a089eade2ed075d0879122a7f31
Théo VINCENT <theo.vincent@eleves.enpc.fr>
2020-05-18 10:55:38 +0200

    Imports change

--- src/data_generation/data_loader.py
+++ src/data_generation/data_loader.py
@@ -2,8 +2,8 @@ from pathlib import Path
 import random as rd
 import numpy as np
 import cv2
-from src.data_generation.data_generator import DataGenerator
-from src.data_generation.transformations import transform_image, transform_label, augmenting
+# from src.data_generation.data_generator import DataGenerator
+from transformations.transformations import transform_image, transform_label, augmenting
 from tensorflow.keras.utils import Sequence
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
@@ -76,10 +76,10 @@ if __name__ == "__main__":
     PATH_LABEL = Path("../../output/test/vid1.csv")
     PERCENTAGE = [0.5, 0.5]
 
-    GENERATOR = DataGenerator(PATH_DATA, PATH_LABEL, percentage=PERCENTAGE)
+    """GENERATOR = DataGenerator(PATH_DATA, PATH_LABEL, percentage=PERCENTAGE)
     TRAIN_SET = GENERATOR.train
     LOADER = DataLoader(TRAIN_SET)
     (BATCH, LABELS) = LOADER[0]
 
     print(BATCH.shape)
-    print(LABELS.shape)
+    print(LABELS.shape)"""
